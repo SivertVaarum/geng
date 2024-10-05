fe331a3360720b526fe86db56deec8b5a7cb658b
Sivert Vårum <sivertlv@stud.ntnu.no>
2024-10-05 23:19:41 +0200

    cleanup

--- code/map.cs
+++ code/map.cs
@@ -8,25 +8,25 @@ namespace geng
     {
         private int[,] _map = new [,]
         {
-            {1,0,0,0},
+            {1,0,1,0},
             {0,0,0,0},
             {1,0,1,0},
-            {0,0,0,1}
+            {0,1,0,0}
         };
         private void LoadMap()
         { 
-            
+            //TODO, load maps from seperate files that will be written into the _map array.
         }
         public SpriteBatch Draw( SpriteBatch spriteBatch,  Texture2D spriteSheet)
         {   //              0 >  4
-            for(int y = 0; y <= _map.GetLength(1) -1 ; y++ ) //y value of map
+            for(int i = 0; i <= _map.GetLength(1) -1 ; i++ ) //y value of map
             {
-                for(int x = 0; x <= _map.GetLength(0) - 1; x++)//x value of map
+                for(int j = 0; j <= _map.GetLength(0) - 1; j++)//x value of map
                 {
-                    if(_map[x, y] == 1)
+                    if(_map[j, i] == 1)
                     {//Draw(Texture2D texture, Rectangle destinationRectangle, Rectangle? sourceRectangle, Color color)
 
-                        spriteBatch.Draw(spriteSheet, new Rectangle(x*128, y*128, 128, 128), 
+                        spriteBatch.Draw(spriteSheet, new Rectangle(i*128, j*128, 128, 128), 
                                             new Rectangle(0, 0, 1, 1), Color.White);
                     }
                 }
