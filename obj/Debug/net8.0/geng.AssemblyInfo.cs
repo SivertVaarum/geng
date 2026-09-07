8123de9d5a96557793274e69106366303ef86b4c
Sivert Sivertsen <sivertlv3@gmail.com>
2026-09-07 16:01:24 +0200

    Oddy sprettball

--- obj/Debug/net8.0/geng.AssemblyInfo.cs
+++ obj/Debug/net8.0/geng.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("geng")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bc630de1ea31fa9a7bb810cc56663cac869b80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b771992752e580c95875f541e4e5c270a6de6a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("geng")]
 [assembly: System.Reflection.AssemblyTitleAttribute("geng")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
